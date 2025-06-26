df6b355df5ddde34a347e4115d02b97cc2c66343
irfansigns <irfansigns@gmail.com>
2025-06-27 00:22:27 +0500

    Don't Show Login/Register Buttons to Signed In User Conditional Rendering

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 // import { CustomCategory } from "../types";
 import { CategoriesSidebar } from "./categories-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface Props {
   disabled?: boolean;
@@ -13,6 +16,8 @@ interface Props {
 
 export const SearchInput = ({ disabled }: Props) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
 
   return (
     <div className="flex items-center gap-2 w-full">
@@ -37,6 +42,13 @@ export const SearchInput = ({ disabled }: Props) => {
       >
         <ListFilterIcon />
       </Button>
+      {session.data?.user ? (
+        <Button asChild variant="elevated">
+          <Link prefetch href="/sign-up">
+            <BookmarkCheckIcon />
+          </Link>
+        </Button>
+      ) : null}
     </div>
   );
 };
