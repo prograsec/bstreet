bbfc8e50f7c29cb795850a605fb986ec04ddfa46
irfansigns <irfansigns@gmail.com>
2025-06-27 18:06:50 +0500

    Changing Folder Structure

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,8 +1,8 @@
 import { Suspense } from "react";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Navbar } from "./navbar";
-import { Footer } from "./footer";
+import { Navbar } from "@/modules/home/ui/components/navbar";
+import { Footer } from "@/modules/home/ui/components/footer";
 import { SearchFilters, SearchFiltersLoading } from "./search-filters";
 
 interface Props {
