daaaf58415e4d2591f927d1a4c564db3a7c68aa0
irfansigns <irfansigns@gmail.com>
2025-06-27 19:52:12 +0500

    Async Await for Dynamic Routing

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,5 +1,14 @@
-const Page = () => {
-  return <div>Category Category Page</div>;
+interface Props {
+  params: Promise<{
+    //Because it is a dynamic route, the params are not resolved until the page is rendered.
+    //It is a server component, so we need to use async await to get the params.
+    category: string;
+  }>;
+}
+
+const Page = async ({ params }: Props) => {
+  const { category } = await params;
+  return <div>Category: {category}</div>;
 };
 
 export default Page;
